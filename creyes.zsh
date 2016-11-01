572cb47139938d0ec1647f1380c09f19f31dc114
Chris Reyes <ChrisReyes@alum.mit.edu>
2016-10-31 18:31:48 -0700

    Initial commit of dotfiles

---
+++ creyes.zsh
@@ -0,0 +1,35 @@
+### History
+
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+
+# Ignore duplicates in command history by actively deleting the oldest duplicate as commands are typed.
+setopt HIST_IGNORE_ALL_DUPS
+# Expire duplicate entries first when trimming history.
+setopt HIST_EXPIRE_DUPS_FIRST
+# Write to the history file immediately, not when the shell exits.
+setopt INC_APPEND_HISTORY
+# Share history between all sessions.
+setopt SHARE_HISTORY
+# Don't record an entry starting with a space.
+setopt HIST_IGNORE_SPACE
+# Remove superfluous blanks before recording entry.
+setopt HIST_REDUCE_BLANKS
+# Don't execute immediately upon history expansion.
+setopt HIST_VERIFY
+# Beep when accessing nonexistent history.
+setopt HIST_BEEP
+
+### Paths and Other Important Variables
+
+export ANDROID_HOME="$HOME/Library/Android/sdk";
+export PATH="/Applications/LilyPond.app/Contents/Resources/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH";
+
+### Aliases
+
+alias f="fg"
+alias j="jobs"
+alias vimbrc="vim $HOME/custom/creyes.zsh"
+alias vimbp="vim $HOME/custom/creyes.zsh"
+alias .brc=". $HOME/custom/creyes.zsh"
+alias .bp=". $HOME/custom/creyes.zsh"
