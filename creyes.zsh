cedda4fb9dec2e8089f89dadcf8b1fb8daea0368
Chris Reyes <ChrisReyes@alum.mit.edu>
2016-11-01 08:56:25 -0700

    Avoiding export of local variables

--- creyes.zsh
+++ creyes.zsh
@@ -1,36 +1,40 @@
-### History
+function () {
+	### History
 
-export HISTSIZE=10000000
-export SAVEHIST=10000000
+	export HISTSIZE=10000000;
+	export SAVEHIST=10000000
 
-# Ignore duplicates in command history by actively deleting the oldest duplicate as commands are typed.
-setopt HIST_IGNORE_ALL_DUPS
-# Expire duplicate entries first when trimming history.
-setopt HIST_EXPIRE_DUPS_FIRST
-# Write to the history file immediately, not when the shell exits.
-setopt INC_APPEND_HISTORY
-# Share history between all sessions.
-setopt SHARE_HISTORY
-# Don't record an entry starting with a space.
-setopt HIST_IGNORE_SPACE
-# Remove superfluous blanks before recording entry.
-setopt HIST_REDUCE_BLANKS
-# Don't execute immediately upon history expansion.
-setopt HIST_VERIFY
-# Beep when accessing nonexistent history.
-setopt HIST_BEEP
+	# Ignore duplicates in command history by actively deleting the oldest duplicate as commands are typed.
+	setopt HIST_IGNORE_ALL_DUPS;
+	# Expire duplicate entries first when trimming history.
+	setopt HIST_EXPIRE_DUPS_FIRST;
+	# Write to the history file immediately, not when the shell exits.
+	setopt INC_APPEND_HISTORY;
+	# Share history between all sessions.
+	setopt SHARE_HISTORY;
+	# Don't record an entry starting with a space.
+	setopt HIST_IGNORE_SPACE;
+	# Remove superfluous blanks before recording entry.
+	setopt HIST_REDUCE_BLANKS;
+	# Don't execute immediately upon history expansion.
+	setopt HIST_VERIFY;
+	# Beep when accessing nonexistent history.
+	setopt HIST_BEEP;
 
-### Paths and Other Important Variables
+	### Paths and Other Important Variables
 
-export GOPATH="$HOME/golang";
-export ANDROID_HOME="$HOME/Library/Android/sdk"; #TODO: This is for Mac, but need this to also work for Ubuntu
-export PATH="/Applications/LilyPond.app/Contents/Resources/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$GOPATH/bin:$PATH";
+	export GOPATH="$HOME/golang";
+	export ANDROID_HOME="$HOME/Library/Android/sdk"; #TODO: This is for Mac, but need this to also work for Ubuntu
+	export PATH="/Applications/LilyPond.app/Contents/Resources/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$GOPATH/bin:$PATH";
 
-### Aliases
+	### Aliases
 
-alias f="fg"
-alias j="jobs"
-alias vimbrc="vim $HOME/custom/creyes.zsh"
-alias vimbp="vim $HOME/custom/creyes.zsh"
-alias .brc=". $HOME/custom/creyes.zsh"
-alias .bp=". $HOME/custom/creyes.zsh"
+	local zshcustom="$HOME/.oh-my-zsh/custom";
+
+	alias f="fg";
+	alias j="jobs";
+	alias vimbrc="vim $zshcustom/creyes.zsh";
+	alias vimbp="vim $zshcustom/creyes.zsh";
+	alias .brc=". $zshcustom/creyes.zsh";
+	alias .bp=". $zshcustom/creyes.zsh";
+}
