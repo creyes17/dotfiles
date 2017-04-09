21988a3931ecfb3f6b208fa1b781841beefd851c
Chris Reyes <ChrisReyes@alum.mit.edu>
2017-04-09 09:06:56 -0700

    Added lein license

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,5 @@
 {:user {:plugins [[lein-binplus "0.6.2"]
-                  [cider/cider-nrepl "0.14.0"]]}}
+                  [cider/cider-nrepl "0.14.0"]
+                  [lein-license "0.1.6"]]
+        :license {:author "Christopher R Reyes"
+                  :email  "ChrisReyes@alum.mit.edu"}}}
