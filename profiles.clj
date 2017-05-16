4dce3a9e0d6b4eae57145f6f5824a3cd10ebb14f
Chris Reyes <ChrisReyes@alum.mit.edu>
2017-05-15 17:38:23 -0700

    Adding tracing tools

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-binplus "0.6.2"]
                   [cider/cider-nrepl "0.14.0"]
-                  [lein-license "0.1.6"]]
+                  [lein-license "0.1.6"]
+                  [org.clojure/tools.trace "0.7.9"]]
         :license {:author "Christopher R Reyes"
                   :email  "ChrisReyes@alum.mit.edu"}}}
