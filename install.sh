04fcf6b51fc28b50001c8ac948cd724d2aa08987
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2018-02-26 14:26:44 -0800

    Minor updates to install.sh

--- install.sh
+++ install.sh
@@ -23,18 +23,20 @@ usage() {
 		    NONE
 
 		Dependencies
-		    cargo
+		    cargo (via rust)
+		    cmake
 		    curl
 		    git
 		    go
+		    mono
 		    node
 		    npm
 		    python
 		    readlink
-		    rustc
-		    tsserver
+		    reattach-to-user-namespace
+		    rustc (via rust)
+		    tsserver (via typescript)
 		    unzip
-		    xbuild
 		    xcode-select
 
 		Side Effects
@@ -149,9 +151,6 @@ setup_vim() {
 	xcode-select --install;
 	set -e;
 
-	# TODO: Make this idempotent
-	python "$script_dir_abs/third_party/valloric/YouCompleteMe/install.py" --all;
-
 	# TODO: Loop through all the files that should be symlinked in the home directory
 	if [ ! -d "$HOME/.vim" ]; then
 		ln -s "$script_dir_abs/.vim" "$HOME";
@@ -168,6 +167,9 @@ setup_vim() {
 	if [ ! -e "$HOME/.tern-project"]; then
 		ln -s "$script_dir_abs/.tern-project" "$HOME";
 	fi
+
+	# TODO: Make this idempotent
+	python "$script_dir_abs/third_party/valloric/YouCompleteMe/install.py" --all;
 
 	return 0;
 }
