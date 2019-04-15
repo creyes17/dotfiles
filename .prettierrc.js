56c43dfc46821652577dce997e747b22094c0f68
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2019-04-15 15:03:14 -0700

    Adding prettier options

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// These options are from Seesaw
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
